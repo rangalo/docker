c84d351a9031bbb1ae8a63e8e2501e20cfc82325
Hardik Mehta <hardik.mehta@zeppelin.com>
2014-12-22 15:50:57 +0100

    chaning admin passwrod, enabling secure login

--- glassfish4/run.sh
+++ glassfish4/run.sh
@@ -2,5 +2,37 @@
 
 set -e
 
-echo "starting the glassfish server..."
+ADMIN_PASS=${ADMIN_PASS:-secret}
+CH_PWFILE="./.admin_pw_changed"
+
+function change_admin_password() {
+
+    echo "=> Changing admin password..."
+    /change_admin_pass.expect ${ADMIN_PASS}
+    echo "=> Done."
+    echo ""
+    echo "=> Enabling secure admin..."
+    /enable_secure_login.expect ${ADMIN_PASS}
+    echo "=> Done."
+
+    echo "========================================================================"
+    echo "You can now connect to this Glassfish server using:"
+    echo ""
+    echo "     admin:$ADMIN_PASS"
+    echo ""
+    echo "Please remember to change the above password as soon as possible!"
+    echo "========================================================================"
+    
+}
+
+
+if [[ ! -f ${CH_PWFILE} ]]; then
+
+    asadmin start-domain
+    change_admin_password
+    asadmin stop-domain
+fi
+
+echo "=> Starting the glassfish server..."
 asadmin start-domain -w
+echo "=> Done."
